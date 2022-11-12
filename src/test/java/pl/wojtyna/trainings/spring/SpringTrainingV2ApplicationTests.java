bdbe43c8622c5d1fcbf7fce612e00f696c8aa294
Mike Wojtyna <mike@buildmysoftware.pro>
2022-11-12 20:01:40 +0100

    Create an empty project

---
+++ src/test/java/pl/wojtyna/trainings/spring/SpringTrainingV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojtyna.trainings.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
