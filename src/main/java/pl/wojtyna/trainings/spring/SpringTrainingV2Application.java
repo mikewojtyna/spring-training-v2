60626cb03d233c77969cf3ecaab00456a390c094
Mike Wojtyna <mike@buildmysoftware.pro>
2022-11-13 21:43:43 +0100

    Manually register spring beans

--- src/main/java/pl/wojtyna/trainings/spring/SpringTrainingV2Application.java
+++ src/main/java/pl/wojtyna/trainings/spring/SpringTrainingV2Application.java
@@ -1,9 +1,21 @@
 package pl.wojtyna.trainings.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SpringTrainingV2Application {
 
     public static void main(String[] args) {
-        // TODO: write your code here
+        var context = new AnnotationConfigApplicationContext();
+        context.register(CliAdapter.class,
+                         InvestorService.class,
+                         SurnameAwareCliCommandsMapper.class,
+                         InMemoryInvestorRepository.class,
+                         RestClientInvestorProfileService.class,
+                         UnirestClient.class,
+                         HardcodedRestResources.class);
+        context.refresh();
+        var cliAdapter = context.getBean(CliAdapter.class);
+        cliAdapter.run(args);
     }
 
 }
