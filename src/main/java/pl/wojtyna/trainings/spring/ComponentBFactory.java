0d3ae90cdd8e6ab850317995fe3d8423b7a7ef74
Mike Wojtyna <mike@buildmysoftware.pro>
2022-11-13 20:36:27 +0100

    Show how to use factory to work on new instances each time

---
+++ src/main/java/pl/wojtyna/trainings/spring/ComponentBFactory.java
@@ -0,0 +1,8 @@
+package pl.wojtyna.trainings.spring;
+
+public class ComponentBFactory {
+
+    public ComponentB newInstance() {
+        return new ComponentB();
+    }
+}
--- src/main/java/pl/wojtyna/trainings/spring/NoSpringApp.java
+++ src/main/java/pl/wojtyna/trainings/spring/NoSpringApp.java
@@ -7,5 +7,14 @@ public class NoSpringApp {
         container.register(CliAdapter.class, CliAdapter.class);
         var cliAdapter = container.getComponent(CliAdapter.class).orElseThrow();
         cliAdapter.run(args);
+
+        // Answer
+        // Try to cover the scenario in which some component wants to receive new instances of its dependencies. How could you do that? Don't need to write code this time, just share your thoughts with me.
+        container.register(ComponentA.class, ComponentA.class);
+        var componentA = container.getComponent(ComponentA.class).orElseThrow();
+        // Uses new instance each time
+        componentA.doSomethingUsingNewInstanceOfBEachTime();
+        componentA.doSomethingUsingNewInstanceOfBEachTime();
+        componentA.doSomethingUsingNewInstanceOfBEachTime();
     }
 }
