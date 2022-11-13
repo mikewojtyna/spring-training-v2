bd4ab3789754639af73943073dce40db34f6db2f
Mike Wojtyna <mike@buildmysoftware.pro>
2022-11-13 20:09:36 +0100

    Change container to automatically scan classpath for components

--- src/main/java/pl/wojtyna/trainings/spring/NoSpringApp.java
+++ src/main/java/pl/wojtyna/trainings/spring/NoSpringApp.java
@@ -4,15 +4,7 @@ public class NoSpringApp {
 
     public static void main(String[] args) {
         var container = NoSpringDependencyInjectionContainer.create();
-
-        container.register(HardcodedRestResources.class, RestResources.class);
-        container.register(RestClientInvestorProfileService.class, InvestorProfileService.class);
-        container.register(UnirestClient.class, RestClient.class);
-        container.register(SurnameAwareCliCommandsMapper.class, CliCommandsMapper.class);
-        container.register(InvestorService.class, InvestorService.class);
-        container.register(InMemoryInvestorRepository.class, InvestorRepository.class);
         container.register(CliAdapter.class, CliAdapter.class);
-
         var cliAdapter = container.getComponent(CliAdapter.class).orElseThrow();
         cliAdapter.run(args);
     }
--- src/main/java/pl/wojtyna/trainings/spring/NoSpringDependencyInjectionContainer.java
+++ src/main/java/pl/wojtyna/trainings/spring/NoSpringDependencyInjectionContainer.java
@@ -1,23 +1,26 @@
 package pl.wojtyna.trainings.spring;
 
+import org.reflections.Reflections;
+
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
+import static org.reflections.scanners.Scanners.SubTypes;
+
 public class NoSpringDependencyInjectionContainer {
 
     private final ConcurrentMap<Class<?>, ImplementationDetails> allDependencies = new ConcurrentHashMap<>();
+    private final Reflections reflections = new Reflections("pl.wojtyna");
 
     public static NoSpringDependencyInjectionContainer create() {
         return new NoSpringDependencyInjectionContainer();
     }
-    
+
     public <C> Optional<C> getComponent(Class<C> typeOfComponent) {
         var maybeImplementationDetails = Optional.ofNullable(allDependencies.get(typeOfComponent));
         List<?> resolvedDependencies = maybeImplementationDetails.stream()
@@ -41,10 +44,25 @@ public class NoSpringDependencyInjectionContainer {
         if (constructors.length != 1) {
             throw new RuntimeException("There must be only one constructor of given component.");
         }
+        putDependency(typeOfComponent, asType, constructors);
+    }
+
+    private void putDependency(Class<?> typeOfComponent, Class<?> asType, Constructor<?>[] constructors) {
         List<Class<?>> constructorDependencies = Arrays.stream(constructors[0].getParameters())
                                                        .map(Parameter::getType)
                                                        .collect(Collectors.toCollection(ArrayList::new));
+        tryToAutomaticallyRegisterDependencies(constructorDependencies);
         allDependencies.put(asType, new ImplementationDetails(typeOfComponent, constructorDependencies));
+    }
+
+    private void tryToAutomaticallyRegisterDependencies(List<Class<?>> dependencies) {
+        for (Class<?> dependency : dependencies) {
+            Set<Class<?>> subTypes = reflections.get(SubTypes.of(dependency).asClass());
+            subTypes.stream()
+                    .findAny()
+                    .ifPresentOrElse(anySubType -> putDependency(anySubType, dependency, anySubType.getConstructors()),
+                                     () -> putDependency(dependency, dependency, dependency.getConstructors()));
+        }
     }
 
     private record ImplementationDetails(Class<?> concreteType, List<Class<?>> dependencies) {}
